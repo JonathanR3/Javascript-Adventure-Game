a7ff200793e996a25fed16bbfbb3f30467b36a2f
JonathanR3 <94479170+JonathanR3@users.noreply.github.com>
2021-12-13 15:55:48 -0500

    Starting structure of options.

--- js/script.js
+++ js/script.js
@@ -4,11 +4,21 @@ let firstStage = {
     id: 1,
     story: "Gilbert woke up in the middle of a hallway in a mansion and is confused. Get him out of it! ",
     options: ["start heading up the hall", "start heading down the hall"]
-}
-
-let secondStage = {
-    id: 2, 
-}
+} 
+if (firstStage.options == "start heading up the hall") {
+            let secondStage = {
+            id: 2, 
+            story: "Gilbert stumbles upon a weirdly shaped key on the ground. What should he do with it?"
+            options: ["Take it and run down the hallway."]
+        }
+    {
+else if (firstStage.options == "start heading down te hall") {
+            let secondStage = {
+            id: 2, 
+            story:"Gilbert hears something metal like chains drag across the floor.";
+            options: 
+        }
+    {
 
 let thirdStege = {
 
