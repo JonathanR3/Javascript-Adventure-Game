cfcf3811ca8401959bb370a11301ce0f935b1652
JonathanR3 <94479170+JonathanR3@users.noreply.github.com>
2021-12-17 16:55:00 -0500

    Overhaul on old code

--- js/script.js
+++ js/script.js
@@ -1,51 +1,123 @@
 console.log("script is loading");
 
-let firstStage = {
-    id: 1,
-    story: "Gilbert woke up in the middle of a hallway in a mansion and is confused. Get him out of it!",
-    options: ["start heading up the hall", "start heading down the hall"]
-} 
-if (firstStage.options == "start heading up the hall") {
-            let secondStage = {
-            id: 2, 
-            story: "Gilbert stumbles upon a weirdly shaped key on the ground. What should he do with it?",
-            options: ["Take it and run down the hallway.", "leave it and run backwards."];
-        }
-                    if (secondStage.options == "Take it and run down the hallway.") {
-                        let thirdStage = {
-                            id: 3, 
-                            story: "Gilbert discovers a secret door that has air coming through its keyhole.",
-                            options: ["Use the key on the secret door", "Leave the key and the secret door and try to leave through the front entrance."];
-                        }
-            }
-                    else if (secondStage.options == "leave it and run backwards.") {
-                        let thirdStage = {
-                            id: 3,
-                            story: "As Gilbert is running back down, he finds a mysterious trapdoor and lifts the latch.",
-                            options: ["Hop through the trapdoor", "ignore the trapdoor and keep running down the hall"];
-                        
-                        }
-                    
-            }
-     }
-else if (firstStage.options == "start heading down the hall") {
-            let secondStage = {
-            id: 2, 
-            story:"Gilbert hears something metal like chains drag across the floor.",
-            options: ["Start running away and into a room.", "investigate the noise."];
-        }
-                if (secondStage.options == "Start running away and into a room.") {
-                    let thirdStage = {
-                        id: 3,
-                        story: "Gilbert finds a key inside a drawer in the room.",
-                        options: ["bring the key to the main entrance and try to unlock it", "leave the key and go upstairs."]
-                        }
-                }
-                else if (secondStage.options == "investigate the noise.") {
-                    let thirdStage = {
-                        id: 3,
-                        story: "Gilbert discovers a hideous monster wrapped in chains around the corner!",
-                        options: ["Try to fight the monster", "run back quickly"];
-                        }
-                }
-     }
+///obtain container for game description
+const gameDescription = document.querySelector(".txtAdventureDescription");
+const optionButtons = document.getElementsByClassName("button");
+
+let startIndex = 0;
+
+//function that starts the game
+function startAdventure(textAdventureIndex) {
+    console.log("The Adventure has started");
+    showGameOptions(textAdventureIndex);
+}
+
+//function that shows the game description
+function showGameOptions(textAdventureID) {
+    const newGameDescription = textAdventure.find((gameNode) => gameNode.id === textAdventureID);
+
+    if(!newGameDescription.options) {
+        document.getElementById("first_btn").style.display = "none";
+        gameDescription.innerHTML = newGameDescription.message;
+        document.getElementById("second_btn").innerHTML = "Play Again";
+        return;
+    }
+
+    gameDescription.innerText = newGameDescription.dialogue;
+
+    //iterate through buttons to show new text options
+    for (let i = 0; i < newGameDescription.options.length; i++) {
+        optionButtons[i].innerText = newGameDescription.options[i].bottomText;
+    }
+}
+
+function buttonOptions() {
+    const adventureIndex = textAdventure.find((gameNode) => gameNode.id === startIndex);
+
+    for (let i = 0; i < optionButtons.length; i++) {
+        optionButtons.addEventListener("click", function () {});
+    }
+}
+
+function onPlay() {
+    let ids = textAdventure.map((item) => {
+        return item.id;
+    }); 
+
+    if (startIndex > ids.length - 1) {
+        startIndex = 0;
+    }
+
+    showGameOptions(ids[startIndex]);
+}
+
+//functions for button event
+function onNextOption(event) {
+    event.preventDefault();
+    startIndex++;
+    onPlay();
+}
+
+function onPlayAgain(event) {
+    event.preventDefault();
+    document.getElementByIdById("first_btn").style.display = "inline";
+    startIndex = 0;
+    onPlay();
+}
+
+//creates object that holds the game text and keeps track of its state
+const textAdventure = [
+    {
+        id: 1,
+        dialogue: "Gilbert woke up in the middle of a hallway in a mansion and is confused. Get him out of it!",
+        options: [
+            {
+            bottomText: "start heading up the hall", 
+            setState: { continue: true },
+            nextText: 2,
+            },
+            {
+            bottomtext: "start heading down the hall",
+            message: console.log("It's a dead end, try again."),
+            },
+        ],
+    },
+    {
+        id: 2,
+        dialogue: "Gilbert sees a mysterious shaped key on the floor but hears rumbling coming from behind him",
+        options: [
+            {
+                bottomText: "start running right with the key",
+                setState: { continue: true},
+                nextText: 3,
+            },
+            {
+                bottomText: "turn back and investigate the noise",
+                message: console.log("A monster grabs you! Try again"),
+            },
+        ],
+    },
+    {
+        id: 3,
+        dialogue: "Gilbert sees a wide open door in front of him, but also notices a smaller door with a lock on it",
+        options: [
+            {
+                bottomText: "Head through the open door",
+                message: console.log("Try again"),
+            },
+            {
+                bottomText: "Unlock the smaller door with the key",
+                setState: { continue: true},
+                nextText: 4,
+            },
+        ],
+    },
+    {
+        id: 4,
+        dialogue: "In front of Gilbert is a wide open path and",
+        message: console.log("Thank you for playing and helping Gilbert escape!"),
+    },
+];
+
+onPlay();
+
